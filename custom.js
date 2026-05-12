0d345aaa38eece13b3e1395c66c451bf3fac5c94
Shivam Kumar Sah <sahushivam7256@gmail.com>
2026-05-12 20:17:20 +0530

    third commit

--- custom.js
+++ custom.js
@@ -13,11 +13,26 @@
 
     // PROJECT CAROUSEL
     $('.owl-carousel').owlCarousel({
-    	items: 1,
-	    loop:true,
-	    margin:10,
-	    nav:true
-	});
+      items: 1,
+      loop: true,
+      margin: 10,
+      nav: true,
+      dots: true,
+      autoplay: true,
+      autoplayTimeout: 2800,
+      autoplayHoverPause: true,
+      smartSpeed: 700,
+      autoplaySpeed: 700,
+      animateOut: 'fadeOut',
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 1
+        }
+      }
+    });
 
     // SMOOTHSCROLL
     $(function() {
